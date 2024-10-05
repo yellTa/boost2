57706372612272261a3f7c10eab4b904d912b9a8
YellTa <>
2024-10-05 11:11:17 +0900

    feat : Task객체생성
    데이터를 저장할 때 사용하는 객체의 생성자는
    title, date, owner, priority만 생성하고 저장
    -> userId는 auto_increment값
    
    데이터를 조회할 때 사용할 객체의 생성자는 json으로 파싱하기 위해
    오버로딩을 사용해 user_id 추가

---
+++ src/main/java/boost/chaper2/dto/Task.java
@@ -0,0 +1,50 @@
+package boost.chaper2.dto;
+
+
+import java.util.Date;
+
+public class Task {
+    int user_id;
+    String title;
+    Date date;
+    String owner;
+    int priority;
+
+    public Task(){
+
+    }
+    public Task(String title, Date date, String owner, int priority){
+        this.title= title;
+        this.date = date;
+        this.owner = owner;
+        this.priority = priority;
+    }
+
+    public Task(int userId,String title, Date date, String owner, int priority){
+        this.user_id = userId;
+        this.title= title;
+        this.date = date;
+        this.owner = owner;
+        this.priority = priority;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
