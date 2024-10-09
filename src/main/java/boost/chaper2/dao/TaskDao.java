81ec791b1e614b32ebed1c5cb29f753b9afd2f57
YellTa <>
2024-10-09 17:39:33 +0900

    refact : static 블록을 사용한 Driver Connection
    TaskDao.java 에서 static블록을 사용해 Driver Connection을 얻음
    Connection을 닫기위해 closeConnection메서드추가
    cleanUpSQLDriver 메소드를 사용해서 JVM에 등록된 모든 JDBC드라이버를 제거
    
    AppListner.java
    애플리케이션이 종료될 떄 TaskDao.java의 작성한 closeConnection을 이용해 connection 종료
     cleanUpSQLDriver를 이용해 JVM에 등록된 모든 JDBC드라이버 해지

--- src/main/java/boost/chaper2/dao/TaskDao.java
+++ src/main/java/boost/chaper2/dao/TaskDao.java
@@ -1,32 +1,79 @@
 package boost.chaper2.dao;
 
-import boost.chaper2.config.DatabaseManager;
 import boost.chaper2.dto.Task;
+import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
 
 import java.sql.*;
+import java.util.Enumeration;
 
 public class TaskDao {
+    private static Connection conn;
+
+    static {
+        String url = "jdbc:mysql://localhost:3306/tables_in_connectdb";
+        String user = "boost";
+        String password = "boost";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public void saveData(Task task) {
-        Connection conn = null;
         PreparedStatement ps = null;
 
         String sql = "INSERT INTO tasks (title, date, owner, priority) VALUES(?,?,?,?)";
         try {
-            conn = DatabaseManager.getInstance().getConnection();
             ps = conn.prepareStatement(sql);
-
             ps.setString(1, task.getTitle());
             ps.setDate(2, task.getDate());
             ps.setString(3, task.getOwner());
             ps.setInt(4, task.getPriority());
 
             ps.executeUpdate();
-
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            DatabaseManager.getInstance().closeConnection(conn, ps);
+            try {
+                if (ps != null) {
+                    ps.close();
+
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
+                if (!conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+            }
+        }
+    }
+
+    public static void cleanUpSQLDriver() {
+        try {
+            AbandonedConnectionCleanupThread.checkedShutdown();
+            Enumeration<Driver> drivers = DriverManager.getDrivers();
+
+            while (drivers.hasMoreElements()) {
+                Driver driver = drivers.nextElement();
+                try {
+                    DriverManager.deregisterDriver(driver);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
