47a671af245ebddc4c6a950e8c1b40decd41c1c4
YellTa <>
2024-10-10 23:42:40 +0900

    refact: static 블록을 사용해 Connection객체 생성
    static블록을 사용해 JDBC드라이버 로드
    클래스가 로드 될 떄 처음 생성되고 재사용함
    try catch resource 구문을 사용해 자원을 닫아줌
    
    ################
    # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    # 예) feat : 로그인 기능 추가
    
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    ################

--- src/main/java/boost/chaper2/dao/TaskDao.java
+++ src/main/java/boost/chaper2/dao/TaskDao.java
@@ -7,73 +7,38 @@ import java.sql.*;
 import java.util.Enumeration;
 
 public class TaskDao {
-    private static Connection conn;
+    private static String url = "jdbc:mysql://localhost:3306/tables_in_connectdb";
+    private static String user = "boost";
+    private static String password = "boost";
 
     static {
-        String url = "jdbc:mysql://localhost:3306/tables_in_connectdb";
-        String user = "boost";
-        String password = "boost";
-
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(url, user, password);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void saveData(Task task) {
-        PreparedStatement ps = null;
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 
+
+    public void saveData(Task task) {
         String sql = "INSERT INTO tasks (title, date, owner, priority) VALUES(?,?,?,?)";
-        try {
-            ps = conn.prepareStatement(sql);
+
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
             ps.setString(1, task.getTitle());
             ps.setDate(2, task.getDate());
             ps.setString(3, task.getOwner());
             ps.setInt(4, task.getPriority());
-
             ps.executeUpdate();
+
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (ps != null) {
-                    ps.close();
-
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
     }
 
-    public static void closeConnection() {
-        if (conn != null) {
-            try {
-                if (!conn.isClosed()) {
-                    conn.close();
-                }
-            } catch (SQLException e) {
-            }
-        }
-    }
-
-    public static void cleanUpSQLDriver() {
-        try {
-            AbandonedConnectionCleanupThread.checkedShutdown();
-            Enumeration<Driver> drivers = DriverManager.getDrivers();
-
-            while (drivers.hasMoreElements()) {
-                Driver driver = drivers.nextElement();
-                try {
-                    DriverManager.deregisterDriver(driver);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
